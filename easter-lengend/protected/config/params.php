69b88160fc75453ac6289d5d1cab2e9142d27902
hzbdesigner <hzbdesigner@gmail.com>
2013-02-28 00:21:46 +0800

    first commit

---
+++ easter-lengend/protected/config/params.php
@@ -0,0 +1,9 @@
+<?php
+
+// this contains the application parameters that can be maintained via GUI
+return array(
+	// this is displayed in the header section
+	'title'=>'My Yii Blog',
+
+	'copyrightInfo'=>'Copyright &copy; 2009 by My Company.',
+);
