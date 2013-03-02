6dc1df41c3467cacacd39e403a91e1ec1609b39b
hzbdesigner <hzbdesigner@gmail.com>
2013-03-02 19:37:16 +0800

    Signed-off-by: hzbdesigner <hzbdesigner@gmail.com>php debug error-page &errorAction

--- easterlengend/protected/controllers/SiteController.php
+++ easterlengend/protected/controllers/SiteController.php
@@ -23,6 +23,17 @@ class SiteController extends Controller
 	       	$this->render('index');
 	}
 	
+	public function actionError()
+	{
+	    if($error=Yii::app()->errorHandler->error)
+	    {
+	    	if(Yii::app()->request->isAjaxRequest)
+	    		echo $error['message'];
+	    	else
+	        	$this->render('error', $error);
+	    }
+	}
+
 
 
 }
--- easterlengend/protected/views/site/error.php
+++ easterlengend/protected/views/site/error.php
@@ -1,1 +1,12 @@
-³ö´íÀ²£¡£¡£¡
+<?php
+$this->pageTitle=Yii::app()->name . ' - Error';
+$this->breadcrumbs=array(
+	'Error',
+);
+?>
+
+<h2>Error <?php echo $code; ?></h2>
+
+<div class="error">
+<?php echo CHtml::encode($message); ?>
+</div>
