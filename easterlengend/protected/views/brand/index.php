e0277cfccd91ee6eb49748e867e6c87d5121818a
hzbdesigner <hzbdesigner@gmail.com>
2013-03-03 21:48:19 +0800

    fix for rgba with images

--- easterlengend/protected/views/brand/index.php
+++ easterlengend/protected/views/brand/index.php
@@ -5,19 +5,19 @@
 		<div class="container brand">
 
 			<ul class="brand-tabs">
-				<li><a href="<?php echo $this->createUrl('/brand/Karges'); ?>">Karges</a></li>
+				<li class="<?php if ($this->action->id== 'Karges') { echo 'active';} ?>"><a href="<?php echo $this->createUrl('/brand/Karges'); ?>">Karges</a></li>
 				<div class="divider"></div>
-				<li><a href="<?php echo $this->createUrl('/brand/Henkel_Harris') ; ?>">Henkel Harris</a></li>
+				<li class="<?php if ($this->action->id== 'Henkel_Harris') { echo 'active';} ?>"><a href="<?php echo $this->createUrl('/brand/Henkel_Harris') ; ?>">Henkel Harris</a></li>
 				<div class="divider"></div>
-				<li><a href="<?php echo $this->createUrl('/brand/Henredon') ; ?>">Henredon</a></li>
+				<li class="<?php if ($this->action->id== 'Henredon') { echo 'active';} ?>"><a href="<?php echo $this->createUrl('/brand/Henredon') ; ?>">Henredon</a></li>
 				<div class="divider"></div>
-				<li><a href="<?php echo $this->createUrl('/brand/Ourhouse') ; ?>">Ourhouse</a></li>
+				<li class="<?php if ($this->action->id== 'Ourhouse') { echo 'active';} ?>"><a href="<?php echo $this->createUrl('/brand/Ourhouse') ; ?>">Ourhouse</a></li>
 				<div class="divider"></div>
-				<li><a href="<?php echo $this->createUrl('/brand/Maitland_Smith') ; ?>">Maitland Smith</a></li>
+				<li class="<?php if ($this->action->id== 'Maitland_Smith') { echo 'active';} ?>"><a href="<?php echo $this->createUrl('/brand/Maitland_Smith') ; ?>">Maitland Smith</a></li>
 				<div class="divider"></div>
-				<li><a href="<?php echo $this->createUrl('/brand/Jonathan_Charles') ; ?>">Jonathan Charles</a></li>
+				<li class="<?php if ($this->action->id== 'Jonathan_Charles') { echo 'active';} ?>"><a href="<?php echo $this->createUrl('/brand/Jonathan_Charles') ; ?>">Jonathan Charles</a></li>
 				<div class="divider"></div>
-				<li><a href="<?php echo $this->createUrl('/brand/Eastern_Legend') ; ?>">Eastern Legend</a></li>
+				<li class="<?php if ($this->action->id== 'Eastern_Legend') { echo 'active';} ?>"><a href="<?php echo $this->createUrl('/brand/Eastern_Legend') ; ?>">Eastern Legend</a></li>
 			</ul>
 
 			<?php echo $sub_content; ?>
